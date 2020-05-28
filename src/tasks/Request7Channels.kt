acd81c0918920a09e5ee0e69d737903b90a1c3e0
diegomrsantos <diegomrsantos@gmail.com>
2020-05-29 00:06:32 +0200

    Use a channel for communication between concurrent coroutines

--- src/tasks/Request7Channels.kt
+++ src/tasks/Request7Channels.kt
@@ -1,6 +1,7 @@
 package tasks
 
 import contributors.*
+import kotlinx.coroutines.async
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
@@ -11,6 +12,23 @@ suspend fun loadContributorsChannels(
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
     coroutineScope {
-        TODO()
+        val channel = Channel<List<User>>()
+        val repos = service
+            .getOrgRepos(req.org)
+            .also { logRepos(req, it) }
+            .bodyList()
+        for (repo in repos) {
+            launch {
+                val repoContributors = service.getRepoContributors(req.org, repo.name)
+                    .also { logUsers(repo, it) }
+                    .bodyList()
+                channel.send(repoContributors)
+            }
+        }
+        val allUsers = mutableListOf<User>()
+        repeat(repos.size) {
+            allUsers += channel.receive()
+            updateResults(allUsers.aggregate(), it == repos.lastIndex)
+        }
     }
 }
